ec9a46958d96f9662d6d774368da9a2bed43143d
v-turin <vladimir.turin@opencascade.com>
2018-11-08 15:15:59 +0300

    Linux build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
+ln -s $PREFIX/lib/libmve.a elibs/mve/libs/mve
+ln -s $PREFIX/lib/libmve_dmrecon.a elibs/mve/libs/dmrecon
+ln -s $PREFIX/lib/libmve_fssr.a elibs/mve/libs/fssr
+ln -s $PREFIX/lib/libmve_ogl.a elibs/mve/libs/ogl
+ln -s $PREFIX/lib/libmve_sfm.a elibs/mve/libs/sfm
+ln -s $PREFIX/lib/libmve_util.a elibs/mve/libs/util
+
 mkdir build && cd build
 
-cmake -G "Ninja"                            \
-    -DCMAKE_INSTALL_PREFIX=${PREFIX}        \
-    -DCMAKE_BUILD_TYPE=Release              \
-    -DCMAKE_INSTALL_LIBDIR=lib              \
+cmake -G "Ninja"                                    \
+    -DCMAKE_INSTALL_PREFIX=${PREFIX}                \
+    -DCMAKE_PREFIX_PATH=${PREFIX}                   \
+    -DCMAKE_BUILD_TYPE=Release                      \
+    -DCMAKE_INSTALL_LIBDIR=lib                      \
+    -DEIGEN3_INCLUDE_DIR=${PREFIX}/include/eigen3   \
     ..
 
 ninja install
